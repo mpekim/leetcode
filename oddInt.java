f74ee62edba982f94c2258dc0bb7e226ae73a00b
mpekim_wsu <mpmorley@worcester.edu>
2023-07-06 00:43:05 -0400

    Fixing oddInt code.

--- oddInt.java
+++ oddInt.java
@@ -3,5 +3,6 @@ public class oddInt
     public static void main(String[] args)
     {
         System.out.println("Please enter two values: ");
+        System.out.println("Checking for the count of Odd Integers...");
     }
 }
