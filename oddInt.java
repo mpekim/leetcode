71967d609ec9074bed988091afe21a3a22d82224
mpekim_wsu <mpmorley@worcester.edu>
2023-07-07 00:42:27 -0400

    Implementation for OddInt and uglyNumber.

--- oddInt.java
+++ oddInt.java
@@ -2,7 +2,19 @@ public class oddInt
 {
     public static void main(String[] args)
     {
-        System.out.println("Please enter two values: ");
-        System.out.println("Checking for the count of Odd Integers...");
+        System.out.println("Odd number range between 3 and 7: ");
+        System.out.println(oddIntCount(3, 7));
     }
+
+    public static int oddIntCount(int low, int high){
+            if (low % 2 == 1)
+            {
+                low--;
+            }
+            if (high % 2 == 1)
+            {
+                high++;
+            }
+            return (high - low) / 2;
+        }
 }
---
+++ uglyNumber.java
@@ -0,0 +1,37 @@
+public class uglyNumber {
+    public static void main(String [] args)
+    {
+        System.out.println("Is the number 6 ugly?");
+        System.out.println(uglyNumberCheck(6));
+    }
+
+    public static boolean uglyNumberCheck(int value){
+        if (value <= 0)
+        {
+            return false;
+        }
+        
+        while (value % 2 == 0)
+        {
+            value = value / 2;
+        }
+
+        while (value % 3 == 0)
+        {
+            value = value / 3;
+        }
+
+        while (value % 5 == 0)
+        {
+            value = value / 5;
+        }
+
+        if (value == 1)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
