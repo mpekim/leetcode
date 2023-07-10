125b9b5c311dc2dbc1dcaf487bc348bd043ba95b
mpekim_wsu <mpmorley@worcester.edu>
2023-07-10 06:58:16 -0400

    Implementing solution for uglyNumber.java.

--- uglyNumber.java
+++ uglyNumber.java
@@ -1,33 +1,27 @@
 public class uglyNumber {
-    public static void main(String [] args)
-    {
+    public static void main(String [] args){
         System.out.println("Is the number 6 ugly?");
         System.out.println(uglyNumberCheck(6));
     }
 
     public static boolean uglyNumberCheck(int value){
-        if (value <= 0)
-        {
+        if (value <= 0){
             return false;
         }
         
-        while (value % 2 == 0)
-        {
+        while (value % 2 == 0){
             value = value / 2;
         }
 
-        while (value % 3 == 0)
-        {
+        while (value % 3 == 0){
             value = value / 3;
         }
 
-        while (value % 5 == 0)
-        {
+        while (value % 5 == 0){
             value = value / 5;
         }
 
-        if (value == 1)
-        {
+        if (value == 1){
             return true;
         }
         else{
