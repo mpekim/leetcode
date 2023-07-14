29ee2dd216d930a8e22610c5f96cfbab89f193eb
mpekim_wsu <mpmorley@worcester.edu>
2023-07-14 00:52:14 -0400

    Further changes to palindrome file.

--- palindromeNumber.java
+++ palindromeNumber.java
@@ -1,6 +1,7 @@
 public class palindromeNumber {
    public static void main(String[] args) 
    {
-    System.out.println("Please enter a number: ");
+      System.out.println("Please enter a number: ");
    }
+   // Check to see if the number is a palindrome.
 }
