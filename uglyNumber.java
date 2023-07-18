28f96d6149c8d54aa4d2b9e35389c4c9b2b4f3c1
mpekim_wsu <mpmorley@worcester.edu>
2023-07-18 00:43:39 -0400

    Adding input to uglyNumber.

--- uglyNumber.java
+++ uglyNumber.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class uglyNumber {
     public static void main(String [] args){
         System.out.println("Is the number 6 ugly?");
         System.out.println(uglyNumberCheck(6));
+
+        Scanner input = new Scanner(System.in);
+        int newNumber = input.nextInt();
+
+        System.out.println(uglyNumberCheck(newNumber));
     } // Perhaps add a Scanner for user input.
 
     public static boolean uglyNumberCheck(int value){
