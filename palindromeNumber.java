62440c7f08fcc16938ca0600ae71c8c6aff1afc4
mpekim_wsu <mpmorley@worcester.edu>
2023-07-20 00:44:51 -0400

    Further implementation of palindromeNumber.java.

--- palindromeNumber.java
+++ palindromeNumber.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
+
 public class palindromeNumber {
    public static void main(String[] args) 
    {
       System.out.println("Please enter a number: ");
+      Scanner input = new Scanner(System.in);
+
+      int number = input.nextInt();
+      String convertNumber = convertIntToString(number);
+
+      System.out.println("Is " + convertNumber + " a palindrome? " + isPalindrome(convertNumber));
    }
    // Check to see if the number is a palindrome.
    // Currently, the number will be entered as a string.
 
-   public static boolean isPalindrome(String num1, String num2){
-      if (num1 == num2)
+   public static boolean isPalindrome(String num1){
+      if (num1 == num1)
       {
          return true;
       }
@@ -18,8 +26,17 @@ public class palindromeNumber {
    }
    public static String convertIntToString(int number)
    {
-      String newString = toString(number);
-      // Converts numbers into strings, as needed. (Can this be done with a typecast?)
+      String newString = "";
+
+      while (number % 10 != 0)
+      {
+         newString = newString + (number % 10);
+         number = number / 10;
+      }
+
+      System.out.println("New number string: " + newString);
+
+      // Use a mod loop to get the digits.
       return newString;
    }
 }
