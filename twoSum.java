bce83ea0089f80548954e54d5a13d57f1168aa45
mpekim_wsu <mpmorley@worcester.edu>
2023-07-23 08:15:40 -0400

    More work on twoSum implementation.

--- twoSum.java
+++ twoSum.java
@@ -1,5 +1,37 @@
 public class twoSum {
     public static void main(String[] args){
+        
         System.out.println("Welcome to the TwoSum program.");
+
+        int[] arr = {2, 3, 4, 5};
+        int[] solution = findTwoSum(arr, 5);
+
+        presentSolution(solution[0], solution[1]);
+    }
+
+    public static int[] findTwoSum(int[] list, int answer){
+
+        for (int i = 0; i < list.length; i++)
+        {
+            for (int j = i + 1; j < list.length; j++)
+            {
+                if (list[i] + list[j] == answer)
+                {
+                    System.out.println("Solution found.");
+
+                    int[] solution = {list[i], list[j]};
+
+                    return solution;
+                }
+            }
+        }
+
+        int[] solution = {-1, -1};
+
+        return solution;
+    }
+
+    public static void presentSolution(int sol1, int sol2){
+        System.out.println("Solution to TwoSum: " + "[" + sol1 + "," + sol2 + "]");
     }
 }
