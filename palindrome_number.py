b6556e175fe7dc910076c64b3ab6201306f7bbd5
mpekim_wsu <mpmorley@worcester.edu>
2023-08-03 00:41:38 -0400

    Allowing for various test values based on input.

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,9 +1,11 @@
-print("Welcome to the palindrome_number.py file.")
-
 def palindrome_number(value):
     if (value == value[::-1]):
         return True
     else:
         return False
 
-print("Is the value 12321 a palindrome:" + palindrome_number("12321"))
+print("Welcome to the palindrome_number.py file.")
+number_string = input("Please enter a number value: ")
+
+print("Is the value 12321 a palindrome: " + str(palindrome_number(number_string)))
+# Add an interface, to allow for continuous checking of values.
