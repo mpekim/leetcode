987d2121708c02f5617b3ab43c2945aa79fe6084
mpekim_wsu <mpmorley@worcester.edu>
2023-08-11 22:02:14 -0400

    Functional two_sum.py file (one situation only).

--- two_sum.py
+++ two_sum.py
@@ -1,6 +1,18 @@
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
+# Find out why VSC doesn't like
+# the import statements.
 
 print("Welcome to the two_sum.py file.")
 
 number_list = {8, 6, 7, 5, 3, 0, 9}
+sum = 13
+
+def two_sum(list, sum):
+    for i in list:
+        for j in list:
+            if (i + j == sum):
+                return {i, j}
+    return "Sum not found"
+
+print(two_sum(number_list, sum))
