41cf66d6026c5dc6626496acac67f29054df81be
mpekim_wsu <mpmorley@worcester.edu>
2023-08-24 22:17:43 -0400

    Adding import for Scanner. Plans on creating objects as needed.

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         palindromeNumber p1 = new palindromeNumber();
         p1.run();
+
+        twoSum ts1 = new twoSum();
+        ts1.run();
+
+        // Creating objects, and then running them.
+        // First implementation style is to simply
+        // create objects, and then run them one-by-one.
+
+        // The next step would be to create an interface that
+        // selects the object to be created.
     }
     // Driver function for the other leetcode classes.
 }
