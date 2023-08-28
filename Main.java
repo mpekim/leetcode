0921aa1a724a57463b05dbad6798180db03bd220
mpekim_wsu <mpmorley@worcester.edu>
2023-08-28 08:12:45 -0400

    Main menu works for selecting objects, but only one time.

--- Main.java
+++ Main.java
@@ -2,18 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        palindromeNumber p1 = new palindromeNumber();
-        p1.run();
 
-        twoSum ts1 = new twoSum();
-        ts1.run();
+        System.out.println("Welcome to the Java Leetcode program.");
+        System.out.println("Please select a program to run: ");
+        System.out.println("1. Odd Int");
+        System.out.println("2. Palindrome Number");
+        System.out.println("3. Two Sum");
+        System.out.println("4. Ugly Number");
 
-        // Creating objects, and then running them.
-        // First implementation style is to simply
-        // create objects, and then run them one-by-one.
+        Scanner input = new Scanner(System.in);
+        int menuChoice = input.nextInt();
 
-        // The next step would be to create an interface that
-        // selects the object to be created.
+        switch(menuChoice)
+        {
+            case 1:
+                oddInt o1 = new oddInt();
+                o1.run();
+                break;
+            case 2:
+                palindromeNumber p1 = new palindromeNumber();
+                p1.run();
+                break;
+            case 3:
+                twoSum t1 = new twoSum();
+                t1.run();
+                break;
+            case 4:
+                uglyNumber u1 = new uglyNumber();
+                u1.run();
+                break;
+            default:
+                System.out.println("I'm sorry, I do not understand that input.");
+                break;
+        }
     }
     // Driver function for the other leetcode classes.
 }
