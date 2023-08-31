8c2857bb1ee0855da40e50b05837ac867996cecc
mpekim_wsu <mpmorley@worcester.edu>
2023-08-30 22:26:38 -0400

    Adding new files to change architecture of C portfolio.

---
+++ c/c_code.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_input(void);
+
+void clear_keyboard_buffer(void);
+// Barebones functions used throughout all programs.
+
+int get_odd_range(int low, int high);
+// Declarations for odd_int.c
+
+int check_palindrome(int number);
+// Declarations for palindrome_number.c
+
+int* get_two_sum(int* list);
+// Declarations for two_sum.c
---
+++ c/main.c
@@ -0,0 +1,7 @@
+# include "c_code.h"
+
+int main(int argc, char* argv[])
+{
+    // Driver for all files.
+    return 0;
+}
