3dbdb7b95c06a0bfe80ef04fdaa6787924d83f75
mpekim_wsu <mpmorley@worcester.edu>
2023-09-03 09:48:12 -0400

    NEW FILE: gen_func.c.

---
+++ c/gen_func.c
@@ -0,0 +1,26 @@
+# include "c_code.h"
+
+int get_input(void)
+{
+    int input;
+    int num_of_conversions;
+
+    num_of_conversions = scanf("%d", &input);
+    clear_keyboard_buffer();
+
+    while (num_of_conversions =! 1)
+    {
+        printf("I'm sorry, I do not understand that input. Please try again.\n");
+        num_of_conversions = scanf("%d", &input);
+        clear_keyboard_buffer();
+    }
+}
+
+void clear_keyboard_buffer(void)
+{
+    char c;
+    do{
+        scanf("%c", &c);
+    } while (c != '\n');
+    return;
+}
