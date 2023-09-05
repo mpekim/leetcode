c73e24b3977091a606a476caaa7080b30bfaf6e3
mpekim_wsu <mpmorley@worcester.edu>
2023-09-04 22:30:38 -0400

    Fixing issues within c_code.h file.

--- c/c_code.h
+++ c/c_code.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum bool
+{
+    FALSE,
+    TRUE
+};
+
+typedef enum bool Bool;
+
 int get_input(void);
 
 void clear_keyboard_buffer(void);
@@ -9,7 +17,7 @@ void clear_keyboard_buffer(void);
 int get_odd_range(int low, int high);
 // Declarations for odd_int.c
 
-int check_palindrome(int number);
+Bool check_palindrome(int number);
 // Declarations for palindrome_number.c
 
 int* get_two_sum(int* list);
