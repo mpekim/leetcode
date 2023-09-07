ea02e9f4068b9347ed63506b1595f01483a3b9a0
mpekim_wsu <mpmorley@worcester.edu>
2023-09-06 21:47:22 -0400

    Adding more functions to palindrome_number code. (Untested)

--- c/c_code.h
+++ c/c_code.h
@@ -18,6 +18,7 @@ int get_odd_range(int low, int high);
 // Declarations for odd_int.c
 
 Bool check_palindrome(int number);
+char* create_num_string(int number);
 // Declarations for palindrome_number.c
 
 int* get_two_sum(int* list, int sum);
--- c/palindrome_number.c
+++ c/palindrome_number.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "c_code.h"
 
-int main(int argc, char* argv[])
+Bool check_palindrome(int number)
 {
-    return 0;
+    // First, convert the number into a string.
+    char* num_string;
+
+    return FALSE;
+}
+
+char* create_num_string(int number)
+{
+    char* num_string;
+    num_string = (char*)malloc(sizeof(char) * 9);
+
+    if (num_string == NULL)
+    {
+        printf("Error: unable to allocate memory for number string. Ending program...\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        num_string[i] = '0';
+    }
+
+    return num_string;
 }
