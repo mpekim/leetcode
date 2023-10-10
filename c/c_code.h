cb72a5ffd6d0121c1844d354aa068260bebf3c48
mpekim_wsu <mpmorley@worcester.edu>
2023-10-10 00:39:03 -0400

    Adding implementation to two_sum program.

--- c/c_code.h
+++ c/c_code.h
@@ -10,10 +10,10 @@ enum bool
 typedef enum bool Bool;
 
 int get_input(void);
-
 void clear_keyboard_buffer(void);
 // Barebones functions used throughout all programs.
 
+void run_oi(void);
 int get_odd_range(int low, int high);
 // Declarations for odd_int.c
 
@@ -23,5 +23,6 @@ char* create_num_string(int number);
 // Declarations for palindrome_number.c
 
 void run_ts(void);
-int* get_two_sum(int* list, int sum);
+int* create_int_list(void);
+int* get_two_sum(int* list, int sum, int size);
 // Declarations for two_sum.c
--- c/two_sum.c
+++ c/two_sum.c
@@ -2,13 +2,56 @@
 
 void run_ts(void)
 {
+    print("Welcome to the Two Sum program.\n");
+    int* list = create_new_list();
+
+    
     return;
+}
+
+int* create_int_list(void)
+{
+    int size;
+    int* new_list;
+
+    printf("Please enter a value for the size of the list: ");
+
+    size = get_input();
+    new_list = (int*)malloc(sizeof(int) * size);
+
+    if (new_list == NULL)
+    {
+        printf("Error: unable to allocate memory for list. Ending program...\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Please enter a value for list item %d: ", (i + 1));
+        new_list[i] = get_input();
+    }
+
+    return new_list;
 }
 
 int* get_two_sum(int* list, int sum, int size)
 {
+    int solution[] = {-1, -1};
+
    for (int i = 0; i < size; i++)
    {
-    
+        for (int j = i; j < size; j++)
+        {
+            if (list[i] + list[j] == sum)
+            {
+                solution[0] = i;
+                solution[1] = j;
+
+                return solution;
+            }
+        }
    }
+   printf("Error: solution not found.\n");
+
+   return solution;
 }
