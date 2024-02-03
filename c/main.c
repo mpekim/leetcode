54f592cba729f52858e8c81fe76dddf1900d2490
Mike Morley <mpekim@gmail.com>
2024-02-02 23:23:55 -0500

    Fixing indentation issues.

--- c/main.c
+++ c/main.c
@@ -4,13 +4,13 @@ int main(int argc, char* argv[])
 {
     // Driver for all files.
     do{
-    int choice;
-    int num_of_conversions = 0;
+        int choice;
+        int num_of_conversions = 0;
 
-    printf("Welcome to the Leetcode C-interface.\n");
-    print("Please enter the program that you would like to run: ");
-    present_choice_menu();
-    choice = get_input();
+        printf("Welcome to the Leetcode C-interface.\n");
+        print("Please enter the program that you would like to run: ");
+        present_choice_menu();
+        choice = get_input();
 
     while(num_of_conversions != 1)
     {
@@ -20,21 +20,21 @@ int main(int argc, char* argv[])
         choice = get_input();
     }
 
-    switch(choice)
-    {
-        case 1:
-            run_oi();
-            break;
-        case 2:
-            run_pn();
-            break;
-        case 3:
-            run_ts();
-            break;
-        default:
-            printf("I'm sorry, I don't understand that input.\n");
-            break;
-    }
+        switch(choice)
+        {
+            case 1:
+                run_oi();
+                break;
+            case 2:
+                run_pn();
+                break;
+            case 3:
+                run_ts();
+                break;
+            default:
+                printf("I'm sorry, I don't understand that input.\n");
+                break;
+        }
     } while (continue_program() == TRUE);
 
     return 0;
