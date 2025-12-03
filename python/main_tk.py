dbbc4c25ba9080e9730ced3c8cc1fa4deb9e5df5
Mike Morley <mpekim@gmail.com>
2025-12-02 21:04:10 -0500

    First step towards a functional interface, with 1 working button.

--- python/main_tk.py
+++ python/main_tk.py
@@ -1,1 +1,21 @@
 # Main module being run as a Tkinter program for user friendly GUI.
+import tkinter as tk
+import num_stud_ute as nsu
+import odd_int as oi
+
+root = tk.Tk()
+
+message = tk.Label(root, text = "Welcome to " \
+"the Tkinter Python GUI for Leetcode programs.\n" \
+"Click a button to run a program.")
+message.pack()
+
+button_num_stud = tk.Button(root, text = "Num Stud UTE",
+                             command = nsu.unable_to_eat)
+button_odd_int = tk.Button(root, text = "Odd Int",
+                             command = oi.run_program)
+
+button_num_stud.pack(padx = 30, pady = 30)
+button_odd_int.pack(padx = 30, pady = 30)
+
+root.mainloop()
